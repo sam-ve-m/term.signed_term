c141a12e1b76a3af20a267d2cfdf75191127f122
Marco Sievers de Almeida <msa@lionx.com.br>
2022-10-11 09:11:52 -0300

    feat: change route an add news terms

--- func/src/domain/enums/terms.py
+++ func/src/domain/enums/terms.py
@@ -8,12 +8,23 @@ class TermsFileType(Enum):
     TERM_NON_COMPLIANCE = "term_non_compliance"
     TERM_RETAIL_LIQUID_PROVIDER = "term_retail_liquid_provider"
 
-    TERM_OPEN_ACCOUNT_DW = "term_open_account_dw"
-    TERM_APPLICATION_DW = "term_application_dw"
-    TERM_PRIVACY_POLICY_AND_DATA_SHARING_POLICY_DW = (
-        "term_and_privacy_policy_data_sharing_policy_dw"
+    TERM_AND_PRIVACY_POLICY_DATA_SHARING_POLICY_DL_PT = (
+        "term_and_privacy_policy_data_sharing_policy_dl_pt"
     )
-    TERM_DISCLOSURES_AND_DISCLAIMERS = "term_disclosures_and_disclaimers"
-    TERM_MONEY_CORP = "term_money_corp"
+    TERM_AND_PRIVACY_POLICY_DATA_SHARING_POLICY_DL_US = (
+        "term_and_privacy_policy_data_sharing_policy_dl_us"
+    )
+    TERM_OPEN_ACCOUNT_DL_PT = "term_open_account_dl_pt"
+    TERM_OPEN_ACCOUNT_DL_US = "term_open_account_dl_us"
+    TERM_BUSINESS_CONTINUITY_PLAN_DL_PT = "term_business_continuity_plan_dl_pt"
+    TERM_BUSINESS_CONTINUITY_PLAN_DL_US = "term_business_continuity_plan_dl_us"
+    TERM_CUSTOMER_RELATIONSHIP_SUMMARY_DL_PT = (
+        "term_customer_relationship_summary_dl_pt"
+    )
+    TERM_CUSTOMER_RELATIONSHIP_SUMMARY_DL_US = (
+        "term_customer_relationship_summary_dl_us"
+    )
+    TERM_ALL_AGREEMENT_GRINGO_DL = "term_all_agreement_gringo_dl"
+
     TERM_GRINGO_WORLD = "term_gringo_world"
     TERM_GRINGO_WORLD_GENERAL_ADVICES = "term_gringo_world_general_advices"
